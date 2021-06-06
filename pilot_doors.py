41eefc7d0082e2f3b3838a42014a7cbe9fa82e75
Wil <molina.wilfredo.j@gmail.com>
2021-06-05 23:38:40 -0500

    fix crash

--- pilot_doors.py
+++ pilot_doors.py
@@ -6,9 +6,9 @@ import Sparks
 from DefFuncs import move_gate
 
 gate_1 = Bladex.CreateEntity(
-    "gate_1", "Rastrillo10", 5284.0770509, -2366.91968365, 1506.1086106, "Physic"
+    "gate_1", "Rastrillo10", 6884.0770509, -2916.91968365, 2006.1086106, "Physic"
 )
-gate_1.Scale = 0.8
+gate_1.Scale = 1
 gate_1.Orientation = (0.500000059605, 0.500000059605, -0.500000059605, 0.499999821186)
 gate_1.Frozen = 1
 gate_1 = Sparks.SetMetalSparkling(gate_1.Name)
@@ -18,13 +18,8 @@ gate_1_din = Objects.CreateDinamicObject(gate_1.Name)
 gate_1_lever = Levers.PlaceLever(
     "gate_1_lever",
     Levers.LeverType3,
-    (4864.20330698, -1495.59549893, 3494.25367907),
-    (
-        0.500000059605,
-        0.500000059605,
-        0.499999910593,
-        -0.499999970198,
-    ),
+    (5864.20330698, -1495.59549893, 4454.25367907),
+    (0.500000059605, 0.500000059605, 0.499999910593, -0.499999970198),
     0.8,
 )
 gate_1_lever.mode = 0
